f41c745718f2ea86593e89422935d258d6cc6f66
Marcelo Briones <ing@marcelobriones.com.ar>
2019-12-07 16:22:28 -0300

    Split homepage

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Menu from "../../components/menu/menu.component";
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className="homePage">
+        <Menu/>
+    </div>
+);
+
+export default HomePage;
