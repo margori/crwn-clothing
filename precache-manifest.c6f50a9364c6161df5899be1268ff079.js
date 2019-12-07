bf211eeb794a9344c6e225d4d31a50c8fcfada65
Marcelo Briones <ing@marcelobriones.com.ar>
2019-12-07 13:49:25 -0300

    Updates

---
+++ precache-manifest.c6f50a9364c6161df5899be1268ff079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5503b7d9c95ec9b099fba807cdad4c04",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "6552790fe9c9ad3d063b",
+    "url": "/crwn-clothing/static/css/main.21943c5e.chunk.css"
+  },
+  {
+    "revision": "92946774df9a2278ae0e",
+    "url": "/crwn-clothing/static/js/2.4132db81.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/crwn-clothing/static/js/2.4132db81.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6552790fe9c9ad3d063b",
+    "url": "/crwn-clothing/static/js/main.8472f122.chunk.js"
+  },
+  {
+    "revision": "8e1f379f0858b5b017bc",
+    "url": "/crwn-clothing/static/js/runtime-main.01128f07.js"
+  }
+]);
