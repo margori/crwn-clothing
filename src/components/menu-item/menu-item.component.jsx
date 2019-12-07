c68c41b020be84d3456d819f0276578f1b92e5d5
Marcelo Briones <ing@marcelobriones.com.ar>
2019-12-07 16:55:00 -0300

    Improve styling

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -7,13 +7,16 @@ const MenuItem = ({
                       imageUrl,
                       size
                   }) => (
-    <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }} className={`menu-item ${size}`}>
-        <div className="content">
-            <h1 className="title">{title}</h1>
-            <span className="subtitle">Shop now</span>
-        </div>
+    <div className={`menu-item ${size}`}>
+        <div className='background-image' style={{
+            backgroundImage: `url(${imageUrl})`
+        }}/>
+
+            <div className="content">
+                <h1 className="title">{title.toUpperCase()}</h1>
+                <span className="subtitle">SHOP NOW}</span>
+            </div>
+
     </div>
 );
 
