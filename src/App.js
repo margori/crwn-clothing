9cdae14ba3c2d3bc9b37d5f05b5eea63c11a94e7
Marcelo Briones <ing@marcelobriones.com.ar>
2019-12-08 19:29:30 -0300

    Add Collection Preview and Item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import {Route, Switch} from "react-router";
+
 import './App.css';
+
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
+        <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
       <HomePage />
+        </Switch>
     </div>
   );
 }
