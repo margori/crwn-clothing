a1455c8919ff2cfa042159e4650de681d89573ba
Marcelo Briones <ing@marcelobriones.com.ar>
2019-12-12 01:37:44 -0300

    Add firebase

--- src/App.js
+++ src/App.js
@@ -7,18 +7,43 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignOutPage from "./pages/sign-in-sign-out/sign-in-sign-out.component";
+import {auth} from "./firebase/firebase.utils";
 
-function App() {
-    return (
-        <div>
-            <Header/>
-            <Switch>
-                <Route exact path="/" component={HomePage}/>
-                <Route path="/shop" component={ShopPage}/>
-                <Route path="/signin" component={SignInAndSignOutPage}/>
-            </Switch>
-        </div>
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUser: null
+        }
+    }
+
+    unsubscribeFromAuth = null;
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(
+            user => {
+                this.setState({currentUser: user})
+            });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
+    render() {
+        return (
+
+            <div>
+                <Header/>
+                <Switch>
+                    <Route exact path="/" component={HomePage}/>
+                    <Route path="/shop" component={ShopPage}/>
+                    <Route path="/signin" component={SignInAndSignOutPage}/>
+                </Switch>
+            </div>
+        )
+            ;
+    }
 }
 
 export default App;
